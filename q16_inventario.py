6a78b4d742935570e9b365c7d9e516dbff20dd90
Johab Freitas <freitasjohab@gmail.com>
2025-12-09 11:37:01 -0300

    :sparkles: feat:utilizando o iloc

--- q16_inventario.py
+++ q16_inventario.py
@@ -25,4 +25,32 @@ print(df.info())
 print(50 * '-')
 
 print('# Passo 2 - Engenharia de Features')
+df['Estoque_Atual'] = (df['Estoque_Inicial'] - df['Unidades_Vendidas'] + df['Unidades_Devolvidas'])
+df['Valor_Total_Estoque'] = (df['Estoque_Atual'] * df['Preco_Custo'])
+print(df)
+print(df.info())
+print(50 * '-')
 
+print('# Passo 3 - Análise de Custo')
+total_estoque = df.groupby('Categoria')['Valor_Total_Estoque'].sum()
+print(total_estoque)
+print(50 * '-')
+
+print('# Passo 4: Amostra de Auditoria')
+df_amostra = df.iloc[[0,1,2], [1,3,7]]
+print(df_amostra)
+print(50 * '-')
+
+print("# Reodernando dataframe")
+# 1. Obter a lista atual de colunas
+colunas = df.columns.tolist()
+print(colunas)
+# 2. Definir a nova ordem desejada
+nova_ordem = ['SKU', 'Produto', 'Categoria', 'Estoque_Inicial', 'Unidades_Vendidas', 'Unidades_Devolvidas', 'Estoque_Atual', 'Preco_Custo', 'Valor_Total_Estoque']
+# 3. Reindexar o DataFrame com a nova ordem
+df_reordenado = df[nova_ordem]
+print("\nDataFrame Reordenado:")
+print(df_reordenado.info())
+
+df_amostra = df_reordenado.iloc[[0,1,2], [1,3,6]]
+print(df_amostra)
